4fbb3d78a363c85676a594c8b119c949d19f10db
ZoeLouise333 <67964814+ZoeLouise333@users.noreply.github.com>
2023-08-07 19:32:35 +0100

    add constants

---
+++ constants/images.js
@@ -0,0 +1,14 @@
+export default [
+  require("../public/images/bumblex.png"),
+  require("../images/5.jpg"),
+  require("../pages/portfolio/images/elarax.png"),
+  require("../pages/portfolio/images/figgyx.png"),
+  require("../pages/portfolio/images/freyax.png"),
+  require("../pages/portfolio/images/cookiex.png"),
+  require("../pages/portfolio/images/darlax.png"),
+  require("../pages/portfolio/images/elarax.png"),
+  require("../pages/portfolio/images/figgyx.png"),
+  require("../pages/portfolio/images/freyax.png"),
+  require("../pages/portfolio/images/bumblex.png"),
+  require("../pages/portfolio/images/bumblex.png"),
+];
