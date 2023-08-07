f0507e05f8878f1e1bccca6edd08083f06058c7f
ZoeLouise333 <67964814+ZoeLouise333@users.noreply.github.com>
2023-08-07 19:57:53 +0100

    add reviews and services

---
+++ pages/reviews/index.js
@@ -0,0 +1,307 @@
+import Head from "next/head";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import Link from "next/link";
+
+export default function Aboutme() {
+  return (
+    <>
+      <Head>
+        <title>Dinky Paws | Puppy Services | Somerset</title>
+      </Head>
+      <div className="container">
+        <Header />
+
+        <section class="section1">
+          <div className="">
+            <h2 class="title centerrr">REVIEWS</h2>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class=" mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage one "></div>
+                  </div>
+                  <div class="subSection3 color1 ">
+                    <h4 className="fontCol">
+                      Being back in the office a few days a week, we needed to
+                      find a dog walker for our puppy and I am so glad we went
+                      with Dinky Paws. Zoe has been walking our puppy twice a
+                      week and the service she provides is brilliant! We get a
+                      comprehensive update every time she completes a walk along
+                      with pictures from the walk, and Zoe has been so patient
+                      when things haven’t gone according to plan, which with a
+                      puppy happens fairly often! (coming in to find puppy has
+                      chewed his harness or been sick etc). She has also been
+                      actively training our puppy to walk calmly on the lead and
+                      we have noticed a massive improvement in his behaviour and
+                      response to other dogs which has been a wonderful bonus!!
+                      We cannot recommend Zoe and Dinky paws enough 😊
+                    </h4>
+                    <h3 class="subTitle">- CHARLOTTE</h3>
+                  </div>
+                </div>
+                <div class=" mobileRow">
+                  <div class="subSection3 ">
+                    <div class="reviewImage two"></div>
+                  </div>
+                  <div class="subSection3 color2 b">
+                    <h4 className="fontCol">
+                      Brilliant! I was recommended to contact Zoe - what a great
+                      recommendation. Unfortunately we couldn't do the meet and
+                      greet, but Zoe turned up and took my 3 springers out. She
+                      was mindful of the heat (it was scorching), she took them
+                      for a short walk and kept them in the shade. I will
+                      definitely use Zoe again. If my dogs are happy, I'm happy.
+                      I would recommend 100%
+                    </h4>
+                    <h3 class="subTitle">- MARIA</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class=" mobileRow">
+                  <div class="subSection3 color3">
+                    <h4 className="fontCol">
+                      We share our home with a very spoilt, cheeky and sensitive
+                      little whippet who isn’t always keen on change, but he
+                      absolutely loves his time with Zoe and I know this because
+                      the updates and photos she sends us are so sweet and
+                      genuine. Being hours away from home and receiving a photo
+                      of our pup fast asleep on Zoe’s lap is heart-melting.
+                      Zoe’s communication is always so thorough and professional
+                      (and usually followed by a couple photos of our dog
+                      looking super happy) Dexter has had last minute drop ins,
+                      hour long walks and an overnight sit with Zoe and we have
+                      always come home to a happy and content boy. I cannot
+                      recommend Dinky Paws enough 🐾
+                    </h4>
+                    <h3 class="subTitle">- GEMMA</h3>
+                  </div>
+
+                  <div class="subSection3">
+                    <div class="reviewImage three"></div>
+                  </div>
+                </div>
+                <div class=" mobileRow">
+                  <div class="subSection3 color4">
+                    <h4 className="fontCol">
+                      Very professional and reliable. Zoe looked after our cats
+                      for two weeks whilst we were away. We received updates and
+                      pictures every morning and knew straight away they would
+                      be in safe hands. We don't live in her area but she went
+                      above and beyond to accommodate us. Highly recommend Zoe,
+                      we will definitely use again.
+                    </h4>
+                    <h3 class="subTitle">- RORY</h3>
+                  </div>
+
+                  <div class="subSection3">
+                    <div class="reviewImage four"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class="mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage five"></div>
+                  </div>
+                  <div class="subSection3 color5">
+                    <h4 className="fontCol">
+                      Zoe looked after my spoilt cat whilst we were on holiday
+                      for a week. We really wanted someone who would spend time
+                      with our older cat and give him lots of attention. Zoe
+                      went above and beyond during her visits. She even offered
+                      to help with watering our plants and closing and opening
+                      curtains. Zoe spent lots of time with our cat and I really
+                      enjoyed getting updates everytime she visited ( with
+                      photos). After using a well known larger company
+                      previously who only spent 3 minutes with our cat (
+                      observed on our ring camera) I was reassured that Zoe was
+                      spending a good amount of time giving our cat lots of fuss
+                      ( 30 quality minutes 🙂 ).It was lovely to know he was
+                      being so well cared for. I would highly recommend Zoe for
+                      pet sitting, she clearly loves animals.
+                    </h4>
+                    <h3 class="subTitle">- ABIGAIL</h3>
+                  </div>
+                </div>
+
+                <div class="mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage six"></div>
+                  </div>
+                  <div class="subSection3 color6">
+                    <h4 className="fontCol">
+                      {" "}
+                      Zoe has been walking and sitting Maggie and Florence now
+                      for a couple of months. They really like her visits and
+                      she keeps us updated with lots of photos and messages. Zoe
+                      is really friendly and responsive to requests.
+                    </h4>
+                    <h3 class="subTitle">- KATIE</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class="mobileRow">
+                  <div class="subSection3 color7">
+                    <h4 className="fontCol">
+                      We would highly recommend Zoe to anyone looking for a
+                      friendly caring person to walk or look after their pet.
+                      Our pup Maggie loves Zoe, and gets very excited to see her
+                      for play and cuddles. Zoe checked in with us while we were
+                      out letting us know that Maggie was fine and also sent us
+                      some lovely pictures of her.
+                    </h4>
+                    <h3 class="subTitle">- EMMA & PAUL</h3>
+                  </div>
+
+                  <div class="subSection3">
+                    <div class="reviewImage seven"></div>
+                  </div>
+                </div>
+
+                <div class="mobileRow">
+                  <div class="subSection3 color8">
+                    <h4 className="fontCol">
+                      Absolutely spoils my little guy during drop ins and
+                      walkies. Has taken time to build his confidence with
+                      people coming to the house and he is always excited when
+                      she arrives for walkies. I get great photos of his
+                      adventures and kept updated of his antics during the day
+                      would highly recommend!
+                    </h4>
+                    <h3 class="subTitle">- EMMA</h3>
+                  </div>
+
+                  <div class="subSection3">
+                    <div class="reviewImage eight"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class="mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage nine"></div>
+                  </div>
+                  <div class="subSection3 color9">
+                    <h4 className="fontCol">
+                      Zoe is absolutely amazing with our new puppy, she has
+                      accommodated us in terms of times and visitsVery reliable,
+                      Cooper looks forward to her visiting, not only does she
+                      take him out but re-enforces all aspects of his training.
+                      I wouldn’t know what to do without her help now
+                      😊🐶throughly recommended.
+                    </h4>
+                    <h3 class="subTitle">- PENNY</h3>
+                  </div>
+                </div>
+
+                <div class="mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage ten"></div>
+                  </div>
+                  <div class="subSection3 color10">
+                    <h4 className="fontCol">
+                      I would highly recommend, my little lady loves her walks
+                      with Zoe. Zoe is also very skilled at dog training and
+                      following consistency so our lady learns and doesn’t get
+                      mixed messages. Very personable service and adapts to what
+                      ever is needed on each visit. Zoe also sends up to date
+                      pics and communicates after each visit.
+                    </h4>
+                    <h3 class="subTitle">- HANNAH</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class="mobileRow">
+                  <div class="subSection3 color11">
+                    <h4 className="fontCol">
+                      I would highly recommend Dinky Paws. Zoe has a lovely
+                      manner she is friendly knowledgeable and very
+                      professional. During our meet and greet she put my mind at
+                      ease and it didn't feel like we were leaving Marley with a
+                      stranger but a friend. And most importantly Marley loved
+                      her straight away. Zoe adds in some training aswell as
+                      walking and has been very flexible for when we have needed
+                      her. so glad we found Dinky Paws. Thank you Zoe🐾
+                    </h4>
+                    <h3 class="subTitle">- KERRY</h3>
+                  </div>
+
+                  <div class="subSection3">
+                    <div class="reviewImage eleven"></div>
+                  </div>
+                </div>
+
+                <div class="mobileRow">
+                  <div class="subSection3 color12">
+                    <h4 className="fontCol">
+                      A very knowledgeable, caring and reliable service. We
+                      cannot recommend highly enough.
+                    </h4>
+                    <h3 class="subTitle">- GRAHAM</h3>
+                  </div>
+
+                  <div class="subSection3">
+                    <div class="reviewImage twelve"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="subSection22 ">
+              <div class="mobileColumn">
+                <div class="mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage thirteen"></div>
+                  </div>
+                  <div class="subSection3 color13">
+                    <h4 className="fontCol">
+                      Zoe provides a great and friendly service. She listens and
+                      takes note of what tasks I would like help with and how I
+                      would like things done when she looks after my house cat.
+                      We like to stick to a routine and she adapts to that for
+                      us! 🙂
+                    </h4>
+                    <h3 class="subTitle">- CHLOE</h3>
+                  </div>
+                </div>
+                <div class="mobileRow">
+                  <div class="subSection3">
+                    <div class="reviewImage fourteen"></div>
+                  </div>
+                  <div class="subSection3 color14">
+                    <h4 className="fontCol">
+                      Would highly recommend! Zoe walks our staffie puppy twice
+                      a week and does an excellent job
+                    </h4>
+                    <h3 class="subTitle">- SAM</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+}
---
+++ pages/services/index.js
@@ -0,0 +1,113 @@
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
+export default function Portfolio() {
+  return (
+    <>
+      <Head>
+        <title>Dinky Paws | Puppy Services | Somerset</title>
+      </Head>
+      <div className="container">
+        <Header />
+
+        <section class="section1">
+          <h2 class="title ">SERVICES</h2>
+          <h5 class="info3 black">mileage fees may apply</h5>
+          <h6 class="info4">*£1 per mile*</h6>
+          <div class="subSection">
+            <div class="catSection">
+              <div class="subSection2 pink">
+                <h3 class="subTitle1">SOLO DOG WALKING</h3>
+
+                <h4>£20 - 1 hour</h4>
+                <h4>£17 - 45 minutes</h4>
+                <h4>£14 - 30 minutes</h4>
+
+                <h5>£10 - per additional dog</h5>
+              </div>
+              <div class="dogImage"></div>
+            </div>
+          </div>
+
+          <div class="subSection">
+            <div class="catSection">
+              <div class="sitImage"></div>
+              <div class="subSection2  orange">
+                <h3 class="subTitle2">DOG/PUPPY SITTING</h3>
+
+                <h4>£14 - 30 minutes</h4>
+                <h4>£17 - 45 minutes</h4>
+                <h4>£20 - per hour</h4>
+                <h4>£17 - per hour 4+ hours</h4>
+                <h5>£10 - per additional dog</h5>
+
+                <h5>*can include a walk if required*</h5>
+              </div>
+            </div>
+          </div>
+
+          <div class="subSection">
+            <div class="catSection">
+              <div class="subSection2  teal">
+                <h3 class="subTitle3">CAT SITTING</h3>
+                <h4 class="white">£9 - 20 minutes</h4>
+                <h4 class="white">£11 - 30 minutes</h4>
+                <h5 class="white">
+                  No aditional charge for more than one cat <br />
+                </h5>
+              </div>
+              <div class="catImage"></div>
+            </div>
+          </div>
+          <div className="pricing__background">
+            <div className="pricing__text nf">
+              <p>
+                <div className="bold of">Before 8am and after 5pm</div>
+                Dogs : Additional £3 per visit/hour
+                <br />
+                Cats : No additional charge
+              </p>
+              <p>
+                <div className="bold of">Weekend + Bank Holiday Rates</div>
+                Dogs : Additional £5 per service/hour <br />
+                Cats : Additional £3 per service
+              </p>
+              <p>
+                <div className="bold of">Milage Fees</div>
+                £1 per mile one way from TN22 area.
+              </p>
+
+              <p>
+                <div>* Please contact me for further information *</div>
+              </p>
+            </div>
+          </div>
+          <div class="subSection">
+            <div class="catSection mb">
+              <div class="nightImage"></div>
+              <div class="subSection2 ssw pink">
+                <h3 class="subTitle2">OVERNIGHT SITTING</h3>
+                <h4>£55 - 9pm till 7am</h4>
+                <h4>£5 - additional consecutive hours either side</h4>
+                <h4>£10 - additional non consecutive hour</h4>
+                <h4>£10 - per additional dog</h4>
+                <h4> 3 nights - friday/saturday/sunday only </h4>
+                <h4> week nights - one night only</h4>
+                <h5>* Please contact me for further information*</h5>
+                <h5>
+                  Dogs can only be left up to 4 hours at a time under Dinky Paws
+                </h5>
+
+                <div id="reviews"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <Footer />
+      </div>
+    </>
+  );
+}
