aaf8a1bcc740f7f14a42f45d21f5ddc8c3d84950
ZoeLouise333 <67964814+ZoeLouise333@users.noreply.github.com>
2023-08-10 20:16:47 +0100

    Update masonry grid

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -2,66 +2,9 @@ import Head from "next/head";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import React, { useEffect, useState } from "react";
-import images from "../../constants/images.js";
+import images from "../../constants/images";
 
 export default function Portfolio() {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const changeSelectedImage = (image) => {
-    setSelectedImage(image);
-  };
-
-  const onLeftClick = (e) => {
-    if (e != null) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-    // find the index of the current selected image
-    const index = images.indexOf(selectedImage);
-    if (index > 0) {
-      changeSelectedImage(images[index - 1]);
-    }
-  };
-
-  const onRightClick = (e) => {
-    if (e != null) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-
-    // find the index of the current selected image
-    const index = images.indexOf(selectedImage);
-    if (index < images.length - 1) {
-      changeSelectedImage(images[index + 1]);
-    }
-  };
-
-  const onCloseClick = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setSelectedImage(null);
-  };
-
-  useEffect(() => {
-    const onKeyDown = (e) => {
-      if (selectedImage != null) {
-        if (e.keyCode === 39) {
-          // move right
-          onRightClick();
-        } else if (e.keyCode === 37) {
-          // move left
-          onLeftClick();
-        }
-      }
-    };
-
-    window.addEventListener("keydown", onKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", onKeyDown);
-    };
-  }, [selectedImage]);
-
   return (
     <>
       <Head>
@@ -69,67 +12,88 @@ export default function Portfolio() {
       </Head>
 
       <Header />
-
-      <div>
-        <div className="centerbb container-offer">
-          <div class="subSection7">
-            <h2 class="title tc white">East Sussex Dogs and Cat Pictures</h2>
-            <h5 class="info3 white">Coming soon... </h5>
-          </div>
+      <div className="backgroundportfolio">
+        <div className="portfoliotitle">East Sussex Dogs and Cats</div>
+        <div class="grid-wrapper">
+          <div className="port onesus big"></div>
+          <div className="port threesus tall"></div>
+          <div className="port twentysevensus tall"></div>
+          <div className="port sevensus"></div>
+          <div className="port fivesus tall"></div>
+          <div className="port sixsus tall"></div>
+          <div className="port eightsus tall"></div>
+          <div className="port ninesus tall"></div>
+          <div className="port twosus big"></div>
+          <div className="port elevensus"></div>
+          <div className="port twelvesus tall"></div>
+          <div className="port thirteensus tall"></div>
+          <div className="port fourteensus tall"></div>
+          <div className="port tensus "></div>
+          <div className="port fifteensus tall "></div>
+          <div className="port sixteensus tall"></div>
+          <div className="port seventeensus tall"></div>
+          <div className="port eighteensus tall"></div>
+          <div className="port nineteensus tall"></div>
+          <div className="port thirtytwosus big"></div>
+          <div className="port twentysus tall"></div>
+          <div className="port twentyonesus tall"></div>
+          <div className="port twentyninesus big"></div>
+          <div className="port twentytwosus tall"></div>
+          <div className="port thirtysus"></div>
+          <div className="port twentyfivesus tall"></div>
+          <div className="port twentysixsus tall"></div>
+          <div className="port foursus tall "></div>
+          <div className="port thirtyonesus tall"></div>
+          <div className="port twentyeightsus tall"></div>
+          <div className="port twentythreesus"></div>
+          <div className="port twentyfoursus"></div>
         </div>
-        <div>
-          <div className="centerbb container-offer">
-            <div class="subSection7">
-              <h2 class="title tc white">Somerset Dogs and Cat Pictures</h2>
-              <h5 class="info3 white">Coming soon... </h5>
-            </div>
-          </div>
-          <br />
-          <br />
-          <br />
-
-          {/*{selectedImage != null && (
-          <div className="lightbox" onClick={() => setSelectedImage(null)}>
-            <a href="#" onClick={onLeftClick}>
-              ←
-            </a>
-            <a href="#" className="closeButton" onClick={onCloseClick}>
-              <img src="/images/icons/close.svg" />
-            </a>
-            {images.map((image) => {
-              return (
-                <img
-                  className="image"
-                  src={image}
-                  style={{ opacity: selectedImage === image ? 1 : 0 }}
-                />
-              );
-            })}
-
-            <a href="#" onClick={onRightClick}>
-              ←
-            </a>
-          </div>
-        )}
-        <div className="masonry__padding">
-          <div className="masonry">
-            {images.map((image) => {
-              return (
-                <div
-                  className="grid"
-                  key={image}
-                  onClick={() => changeSelectedImage(image)}
-                >
-                  <img src={image} />
-                </div>
-              );
-            })}
-          </div>
+        <br />
+        <br />
+        <div className="portfoliotitle">Somerset Dogs and Cats</div>
+        <div class="grid-wrapper">
+          <div className="port onesom tall"></div>
+          <div className="port twosom tall"></div>
+          <div className="port threesom big "></div>
+          <div className="port foursom tall "></div>
+          <div className="port fivesom "></div>
+          <div className="port sixsom tall"></div>
+          <div className="port sevensom tall"></div>
+          <div className="port elevensom wide"></div>
+          <div className="port ninesom tall "></div>
+          <div className="port tensom tall"></div>
+          <div className="port twelvesom tall"></div>
+          <div className="port thirteensom tall "></div>
+          <div className="port seventeensom big"></div>
+          <div className="port fourteensom tall"></div>
+          <div className="port fifteensom tall "></div>
+          <div className="port sixteensom  "></div>
+          <div className="port eighteensom tall"></div>
+          <div className="port nineteensom "></div>
+          <div className="port twentysom tall "></div>
+          <div className="port twentyonesom  "></div>
+          <div className="port eightsom wide "></div>
+          <div className="port twentytwosom big "></div>
+          <div className="port twentythreesom  "></div>
+          <div className="port twentyfoursom "></div>
+          <div className="port twentyfivesom  "></div>
+          <div className="port twentysixsom tall "></div>
+          <div className="port twentysevensom big"></div>
+          <div className="port twentyeightsom tall "></div>
+          <div className="port twentyninesom tall "></div>
+          <div className="port thirtysom  "></div>
+          <div className="port thirtyonesom  "></div>
+          <div className="port thirtytwosom tall"></div>
+          <div className="port thirtythreesom  wide"></div>
+          <div className="port thirtyfoursom  tall"></div>
+          <div className="port thirtyfivesom  tall"></div>
+          <div className="port thirtysixsom  "></div>
+          <div className="port thirtysevensom big"></div>
+          <div className="port thirtyeightsom "></div>
         </div>
-        */}
-        </div>
-        <Footer />
       </div>
+      <Footer />
     </>
   );
 }
+
