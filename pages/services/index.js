e09249f30639e1707820949c3a5ec0b437b9cb4f
ZoeLouise333 <67964814+ZoeLouise333@users.noreply.github.com>
2024-06-26 22:43:11 +0100

    full update

--- pages/services/index.js
+++ pages/services/index.js
@@ -7,22 +7,32 @@ export default function Portfolio() {
   return (
     <>
       <Head>
-        <title>Dinky Paws | Uckfield Dog Walking | East Sussex</title>
+        <title>
+          Dinky Paws | Dog Walking | Cat Sitting | Uckfield | Sussex
+        </title>
       </Head>
       <div className="container">
         <Header />
 
         <section class="section1">
-          <h2 class="title ">SERVICES</h2>
-          <h5 class="info3 black">mileage fees may apply</h5>
-          <h6 class="info4">*£1 per mile*</h6>
+          <h2 class="title margt2 ">SERVICES</h2>
+
+          <h5 class="info3 black ">
+            {" "}
+            Professional friednly dog walker who will love your dog no matter
+            waht breed
+          </h5>
+
           <div class="subSection">
             <div class="catSection">
+              <div className="groupCol">
+                <div class="dogImageD"></div>
+                <div class="dogImageC"></div>
+              </div>
               <div class="subSection2 pink">
-<h3 class="subTitle1">GROUP DOG WALKING</h3>
-
-                <h4>£17 - 1 HOUR</h4>
-              
+                <h3 class="subTitle1"> GROUP DOG WALKING </h3>
+                <h3 class="subbyTitle1">£17 - 1 hour </h3>
+                <h5>* * *</h5>
 
                 <h5> * 1 hour of walking/ running/ playing *</h5>
                 <h5> * professional secured dog cages *</h5>
@@ -42,35 +52,57 @@ export default function Portfolio() {
                 </h5>
 
                 <h5>* tasty kibble as treats *</h5>
-             
+                <h5>* * *</h5>
 
                 <h4 className="groupSpace"> Groups walks are Weekdays only </h4>
                 <h5>£10 - per additional dog from the same household</h5>
               </div>
-              <div class="dogImage"></div>
-            </div>
-          </div>
-
-          <div class="subSection">
-            <div class="catSection">
-              <div class="sitImage"></div>
-              <div class="subSection2  orange">
-                <h3 class="subTitle2">DOG/PUPPY SITTING</h3>
-
-                <h4>£14 - 30 minutes</h4>
-                <h4>£17 - 45 minutes</h4>
-                <h4>£20 - per hour</h4>
-                <h4>£17 - per hour 4+ hours</h4>
-                <h5>£10 - per additional dog</h5>
-
-                <h5>*can include a walk if required*</h5>
+              <div className="groupCol">
+                <div class="dogImageB"></div>
+                <div class="dogImageA"></div>
               </div>
             </div>
           </div>
+          <div className="pricing__background">
+            <div className="pricing__text nf">
+              <p>
+                <div className="bold of">Weekend + Bank Holiday Rates</div>
+                Dogs : Additional £10 per service/hour <br />
+                Cats : Additional £5 per service
+                <br />
+                <br />
+              </p>
+              <p>
+                <div className="bold of">Before 8am and after 5pm</div>
+                Dogs : Additional £5 per visit/hour
+                <br />
+                Cats : No additional charge
+                <br />
+                <br />
+              </p>
+              <p>
+                <div className="bold of">Milage Fees</div>
+                £1 per mile one way from TN22 1UY area.
+              </p>
 
+              <p>
+                <div>* Please contact me for further information *</div>
+              </p>
+            </div>
+          </div>
           <div class="subSection">
             <div class="catSection">
-              <div class="subSection2  teal">
+              <div class="subSection222  orange">
+                <h3 class="subTitle2">SOLO WALKS/VISITS</h3>
+
+                <h4> £25 - 1 hour / per hour</h4>
+                <h4>£21 - 45 minutes</h4>
+                <h4>£17 - 30 minutes</h4>
+                <h5>£10 - per additional dog</h5>
+              </div>
+              <div class="sitImage"></div>
+
+              <div class="subSection222  teal">
                 <h3 class="subTitle3">CAT SITTING</h3>
                 <h4 class="white">£9 - 20 minutes</h4>
                 <h4 class="white">£11 - 30 minutes</h4>
@@ -81,37 +113,15 @@ export default function Portfolio() {
               <div class="catImage"></div>
             </div>
           </div>
-          <div className="pricing__background">
-            <div className="pricing__text nf">
-              <p>
-                <div className="bold of">Before 8am and after 5pm</div>
-                Dogs : Additional £3 per visit/hour
-                <br />
-                Cats : No additional charge
-              </p>
-              <p>
-                <div className="bold of">Weekend + Bank Holiday Rates</div>
-                Dogs : Additional £5 per service/hour <br />
-                Cats : Additional £3 per service
-              </p>
-              <p>
-                <div className="bold of">Milage Fees</div>
-                £1 per mile one way from TN22 area.
-              </p>
 
-              <p>
-                <div>* Please contact me for further information *</div>
-              </p>
-            </div>
-          </div>
           <div class="subSection">
             <div class="catSection mb">
               <div class="nightImage"></div>
               <div class="subSection2 ssw blue34">
                 <h3 class="subTitle2">OVERNIGHT SITTING</h3>
-                <h4>£55 - 9pm till 7am</h4>
-                <h4>£5 - additional consecutive hours either side</h4>
-                <h4>£10 - additional non consecutive hour</h4>
+                <h4>£60 - 9pm till 7am</h4>
+                <h4>£10 - additional consecutive hours either side</h4>
+                <h4>£15 - additional non consecutive hour</h4>
                 <h4>£10 - per additional dog</h4>
                 <h4> 3 nights - friday/saturday/sunday only </h4>
                 <h4> week nights - one night only</h4>
